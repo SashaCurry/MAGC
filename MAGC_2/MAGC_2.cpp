aa9a8ef2f135132cbdb2f9a3d539829423ba99e8
Aleksandr <100157630+SashaCurry@users.noreply.github.com>
2023-12-06 19:25:19 +0600

    Алгоритм "giant step - baby step"

--- MAGC_2/MAGC_2.cpp
+++ MAGC_2/MAGC_2.cpp
@@ -1,4 +1,240 @@
-﻿changeParameters(pair <cpp_int, cpp_int>& P, cpp_int& s, set <pair <cpp_int, cpp_int>> tablePs, pair <cpp_int, cpp_int>& Q,
+﻿#include "iostream"
+#include "vector"
+#include "string"
+#include "set"
+#include "boost/multiprecision/cpp_int.hpp"
+
+using namespace std;
+using namespace boost::multiprecision;
+
+vector <cpp_int> deg2(cpp_int el, cpp_int n) {
+	vector <cpp_int> res;
+	while (n != 0) {
+		if (n / el == 1) {
+			res.push_back(el);
+			n -= el;
+			el = 1;
+		}
+		else
+			el *= 2;
+	}
+	return res;
+}
+
+
+cpp_int multMod(cpp_int n, cpp_int mod, vector <pair <cpp_int, cpp_int>> lst) {
+	if (lst.size() == 1) {
+		cpp_int res = 1;
+		for (int i = 0; i < lst[0].second; i++)
+			res = res * lst[0].first % mod;
+		return res;
+	}
+	else if (lst[0].second == 1) {
+		cpp_int el = lst[0].first;
+		lst.erase(lst.begin());
+		return (el * multMod(n, mod, lst)) % mod;
+	}
+	else {
+		for (int i = 0; i < lst.size(); i++)
+			if (lst[i].second > 1) {
+				lst[i].first = (lst[i].first * lst[i].first) % mod;
+				lst[i].second /= 2;
+			}
+		return multMod(n, mod, lst);
+	}
+}
+
+
+cpp_int powClosed(cpp_int x, cpp_int y, cpp_int mod) {
+	if (y == 0)
+		return 1;
+
+	vector <cpp_int> lst = deg2(1, y);
+	vector <pair <cpp_int, cpp_int>> xDegs;
+	for (int i = 0; i < lst.size(); i++)
+		xDegs.push_back(make_pair(x, lst[i]));
+
+	cpp_int res = multMod(x, mod, xDegs);
+	return res;
+}
+
+
+cpp_int pow(cpp_int x, cpp_int y) {
+	cpp_int res = 1;
+	for (int i = 0; i < y; i++)
+		res *= x;
+	return res;
+}
+
+
+string binForm(cpp_int x) {
+	string bitter = "";
+	while (x != 0) {
+		bitter = (x % 2 == 0 ? "0" : "1") + bitter;
+		x = x / 2;
+	}
+	if (bitter == "")
+		return "0";
+	return bitter;
+}
+
+
+bool miller_rabin(cpp_int n, int k = 10) {
+	if (n == 0 || n == 1)
+		return false;
+
+	cpp_int d = n - 1;
+	cpp_int s = 0;
+	while (d % 2 == 0) {
+		s++;
+		d = d / 2;
+	}
+
+	cpp_int nDec = n - 1;
+	for (int i = 0; i < k; i++) {
+		cpp_int a = rand() % nDec;
+		if (a == 0 || a == 1)
+			a = a + 2;
+
+		cpp_int x = powClosed(a, d, n);
+		if (x == 1 || x == nDec)
+			continue;
+
+		bool flag = false;
+		for (int j = 0; j < s; j++) {
+			x = (x * x) % n;
+			if (x == nDec) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+			return false;
+	}
+
+	return true;
+}
+
+
+pair <cpp_int, cpp_int> advancedEuclid(cpp_int a, cpp_int b) {
+	if (a < 0 || b < 0)
+		throw string{ "Выполнение невозможно: a < 0 или b < 0" };
+
+	cpp_int q, aPrev = a, aCur = b, aNext = -1;
+	cpp_int xPrev = 1, xCur = 0, xNext;
+	cpp_int yPrev = 0, yCur = 1, yNext;
+	while (aNext != 0) {
+		q = aPrev / aCur;
+		aNext = aPrev % aCur;
+		aPrev = aCur; aCur = aNext;
+
+		xNext = xPrev - (xCur * q);
+		xPrev = xCur; xCur = xNext;
+
+		yNext = yPrev - (yCur * q);
+		yPrev = yCur; yCur = yNext;
+	}
+
+	return make_pair(xPrev, yPrev);
+}
+
+
+cpp_int symbolLegendre(cpp_int a, cpp_int p) {
+	if (a % p == 0)
+		return 0;
+	while (a < 0)
+		a += p;
+	cpp_int res = powClosed(a, (p - 1) / 2, p);
+	return res == 1 ? 1 : -1;
+}
+
+
+cpp_int sqrtFromZp(cpp_int a, cpp_int p) {
+	cpp_int m = 0, q = p - 1;
+	while (q % 2 != 1) {
+		m++;
+		q /= 2;
+	}
+
+	cpp_int b = rand() % p;
+	while (symbolLegendre(b, p) != -1)
+		b = (b + 1) % p;
+
+	vector <cpp_int> kArr;
+	for (int i = 1;; i++) {
+		cpp_int k = 0;
+		while (powClosed(a, pow(2, k) * q, p) != 1)
+			k++;
+		kArr.push_back(k);
+		if (k == 0)
+			break;
+		a = (a * pow(b, pow(2, m - kArr.back()))) % p;
+	}
+
+	cpp_int r = powClosed(a, (q + 1) / 2, p);
+	for (int i = kArr.size() - 2; i >= 0; i--)
+		r = (r * advancedEuclid(pow(b, pow(2, m - kArr[i] - 1)), p).first) % p;
+
+	return r;
+}
+
+
+pair <cpp_int, cpp_int> addPoints(pair <cpp_int, cpp_int> p, pair <cpp_int, cpp_int> q, int a, cpp_int field) {
+	if (p.first == -1 && p.second == -1)
+		return q;
+	if (q.first == -1 && q.second == -1)
+		return p;
+
+	while (p.first < 0)  p.first += field;
+	while (p.second < 0) p.second += field;
+	while (q.first < 0)  q.first += field;
+	while (q.second < 0) q.second += field;
+	p.first %= field, p.second %= field;
+	q.first %= field, q.second %= field;
+
+	cpp_int m, revEl;
+	if (p.first == q.first) {
+		if (p.second != q.second)
+			return make_pair(-1, -1);
+		revEl = advancedEuclid(2 * p.second % field, field).first;
+		m = (3 * p.first * p.first + a) * revEl % field;
+	}
+	else {
+		revEl = advancedEuclid((p.first - q.first + field) % field, field).first;
+		m = (p.second - q.second) * revEl % field;
+	}
+	cpp_int xR = (m * m - p.first - q.first) % field;
+	cpp_int yR = -1 * (p.second + m * (xR - p.first)) % field;
+
+	while (xR < 0) xR += field;
+	while (yR < 0) yR += field;
+	return make_pair(xR, yR);
+}
+
+
+pair <cpp_int, cpp_int> scalarMult(cpp_int n, pair <cpp_int, cpp_int> p, int a, cpp_int field) {
+	string nBin = binForm(n);
+	reverse(nBin.begin(), nBin.end());
+
+	pair <cpp_int, cpp_int> res = make_pair(-1, -1);
+	if (nBin[0] == '1')
+		res = p;
+	for (int i = 1; i < nBin.size(); i++) {
+		p = addPoints(p, p, a, field);
+		if (nBin[i] == '1' && res.first == 0 && res.second == 0)
+			res = p;
+		else if (nBin[i] == '1') {
+			res = addPoints(res, p, a, field);
+			if (res.first == -1 && res.second == -1 && i != nBin.size() - 1)
+				return make_pair(-2, -2);
+		}
+
+	}
+	return res;
+}
+
+
+void changeParameters(pair <cpp_int, cpp_int>& P, cpp_int& s, set <pair <cpp_int, cpp_int>> tablePs, pair <cpp_int, cpp_int>& Q,
 	pair <cpp_int, cpp_int>& R, vector <pair <cpp_int, cpp_int>> pairs_ij, vector <cpp_int>& ms, int a, int b, cpp_int p) {
 	cout << endl;
 	for (;;) {
@@ -144,7 +380,7 @@ genS:
 	if (pow(s, 4) != p)
 		s += 1;
 	cout << "\ns = " << s;
-	if(!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
+	if (!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
 		return 0;
 
 	tablePs = { make_pair(-1, -1) };
@@ -156,7 +392,7 @@ genS:
 	cout << "\nТаблица из 2s + 1 точек: ";
 	for (auto i : tablePs)
 		cout << "(" << i.first << ", " << i.second << ") ";
-	if(!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
+	if (!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
 		return 0;
 
 genQR:
@@ -172,7 +408,7 @@ genQR:
 		cout << "\nТочка Q является некорректной!";
 		return 0;
 	}
-	if(!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
+	if (!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
 		return 0;
 	if (Q.first == -1) {
 		cout << "\nТочка Q является некорректной!";
@@ -193,7 +429,7 @@ genQR:
 	cout << "\nПары (i, j): ";
 	for (int i = 0; i < pairs_ij.size(); i++)
 		cout << "(" << pairs_ij[i].first << ", " << pairs_ij[i].second << ") ";
-	if(!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
+	if (!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
 		return 0;
 
 	cpp_int infinum = p + 1 - 2 * sqrt(p), supremum = p + 1 + 2 * sqrt(p);
@@ -206,7 +442,7 @@ genQR:
 	cout << "\nКандидаты на порядок ЭК: ";
 	for (int i = 0; i < ms.size(); i++)
 		cout << ms[i] << " ";
-	if(!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
+	if (!changeParameters(P, s, tablePs, Q, R, pairs_ij, ms, a, b, p))
 		return 0;
 
 	for (short k = 0; ms.size() > 1 && k < 1000; k++) {
@@ -229,7 +465,7 @@ genQR:
 	for (short i = 1; i < ms.size(); i++)
 		if (ms[i] < mRes)
 			mRes = ms[i];
-	if (scalarMult(mRes, P, a, p).first != -1) { 
+	if (scalarMult(mRes, P, a, p).first != -1) {
 		//cout << "\nРезультат не является порядком ЭК! \n";
 		cout << "\nВ ходе работы параметры были изменены! \n";
 		return 0;
